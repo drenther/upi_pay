b9f3265cf12d498a6cc76b9372e8a443090366e7
Sharad Chand <sharad@sharadchand.com>
2019-11-20 14:51:07 +0530

    Initiate payment

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,19 +3,35 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:upi_pay/upi_pay.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(App());
 
-class MyApp extends StatefulWidget {
+class App extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('UPI Pay'),
+        ),
+        body: Screen(),
+      ),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class Screen extends StatefulWidget {
+  @override
+  _ScreenState createState() => _ScreenState();
+}
+
+class _ScreenState extends State<Screen> {
+  String _upiAddrError;
+
   final _upiAddressController = TextEditingController();
   final _amountController = TextEditingController();
 
   bool _isUpiEditable = false;
-  Future<List<ApplicationWithIcon>> _appsFuture;
+  Future<List<ApplicationMeta>> _appsFuture;
 
   @override
   void initState() {
@@ -40,131 +56,168 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  Future<void> _onTap(ApplicationMeta app) async {
+    final err = _validateUpiAddress(_upiAddressController.text);
+    if (err != null) {
+      setState(() {
+        _upiAddrError = err;
+      });
+      return;
+    }
+    setState(() {
+      _upiAddrError = null;
+    });
+
+    final a = await UpiPay.initiateTransaction(
+      amount: _amountController.text,
+      app: app.upiApplication,
+      receiverName: 'Sharad',
+      receiverUpiAddress: _upiAddressController.text,
+      transactionRef: Random.secure().nextInt(1 << 32).toString(),
+    );
+
+    print(a);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('UPI Pay'),
-        ),
-        body: Container(
-          padding: EdgeInsets.symmetric(horizontal: 16),
-          child: ListView(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(top: 32),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: TextField(
-                        controller: _upiAddressController,
-                        enabled: _isUpiEditable,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'address@upi',
-                          labelText: 'Receiving UPI Address',
-                        ),
-                      ),
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: ListView(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 32),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextFormField(
+                    controller: _upiAddressController,
+                    enabled: _isUpiEditable,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'address@upi',
+                      labelText: 'Receiving UPI Address',
                     ),
-                    Container(
-                      margin: EdgeInsets.only(left: 8),
-                      child: IconButton(
-                        icon: Icon(
-                          _isUpiEditable ? Icons.check : Icons.edit,
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            _isUpiEditable = !_isUpiEditable;
-                          });
-                        },
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 32),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: TextField(
-                        controller: _amountController,
-                        readOnly: true,
-                        enabled: false,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Amount',
-                        ),
-                      ),
+                Container(
+                  margin: EdgeInsets.only(left: 8),
+                  child: IconButton(
+                    icon: Icon(
+                      _isUpiEditable ? Icons.check : Icons.edit,
                     ),
-                    Container(
-                      margin: EdgeInsets.only(left: 8),
-                      child: IconButton(
-                        icon: Icon(Icons.loop),
-                        onPressed: _generateAmount,
-                      ),
-                    ),
-                  ],
+                    onPressed: () {
+                      setState(() {
+                        _isUpiEditable = !_isUpiEditable;
+                      });
+                    },
+                  ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 256),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.only(bottom: 12),
-                      child: Text(
-                        'Pay Using',
-                        style: Theme.of(context).textTheme.caption,
-                      ),
-                    ),
-                    FutureBuilder<List<ApplicationWithIcon>>(
-                      future: _appsFuture,
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState != ConnectionState.done) {
-                          return Container();
-                        }
-
-                        return GridView.count(
-                          crossAxisCount: 2,
-                          shrinkWrap: true,
-                          mainAxisSpacing: 8,
-                          crossAxisSpacing: 8,
-                          childAspectRatio: 1.6,
-                          children: snapshot.data
-                              .map((it) => Material(
-                                    color: Colors.grey[200],
-                                    child: InkWell(
-                                      onTap: () {},
-                                      child: Column(
-                                        mainAxisSize: MainAxisSize.min,
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: <Widget>[
-                                          Image.memory(
-                                            it.icon,
-                                            width: 64,
-                                          ),
-                                          Container(
-                                            margin: EdgeInsets.only(top: 4),
-                                            child: Text(it.appName),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ))
-                              .toList(),
-                        );
-                      },
-                    ),
-                  ],
-                ),
-              )
-            ],
+              ],
+            ),
           ),
-        ),
+          if (_upiAddrError != null)
+            Container(
+              margin: EdgeInsets.only(top: 4, left: 12),
+              child: Text(
+                _upiAddrError,
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+          Container(
+            margin: EdgeInsets.only(top: 32),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    controller: _amountController,
+                    readOnly: true,
+                    enabled: false,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Amount',
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 8),
+                  child: IconButton(
+                    icon: Icon(Icons.loop),
+                    onPressed: _generateAmount,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 256),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(bottom: 12),
+                  child: Text(
+                    'Pay Using',
+                    style: Theme.of(context).textTheme.caption,
+                  ),
+                ),
+                FutureBuilder<List<ApplicationMeta>>(
+                  future: _appsFuture,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState != ConnectionState.done) {
+                      return Container();
+                    }
+
+                    return GridView.count(
+                      crossAxisCount: 2,
+                      shrinkWrap: true,
+                      mainAxisSpacing: 8,
+                      crossAxisSpacing: 8,
+                      childAspectRatio: 1.6,
+                      children: snapshot.data
+                          .map((it) => Material(
+                                key: ObjectKey(it.upiApplication),
+                                color: Colors.grey[200],
+                                child: InkWell(
+                                  onTap: () => _onTap(it),
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Image.memory(
+                                        it.icon,
+                                        width: 64,
+                                        height: 64,
+                                      ),
+                                      Container(
+                                        margin: EdgeInsets.only(top: 4),
+                                        child: Text(it.appName),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ))
+                          .toList(),
+                    );
+                  },
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
+}
+
+String _validateUpiAddress(String value) {
+  if (value.isEmpty) {
+    return 'UPI Address is required.';
+  }
+
+  if (!UpiPay.checkIfUpiAddressIsValid(value)) {
+    return 'UPI Address is invalid.';
+  }
+
+  return null;
 }
