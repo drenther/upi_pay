7aaf7716d60ecd430999c602ce24a5b77df52169
Sharad Chand <sharad@sharadchand.com>
2019-11-29 13:59:29 +0530

    Get the installed upi apps

--- android/src/main/kotlin/com/drenther/upi_pay/UpiPayPlugin.kt
+++ android/src/main/kotlin/com/drenther/upi_pay/UpiPayPlugin.kt
@@ -1,6 +1,7 @@
 package com.drenther.upi_pay
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.util.Log
 import io.flutter.plugin.common.MethodCall
@@ -22,51 +23,85 @@ class UpiPayPlugin internal constructor(registrar: Registrar, channel: MethodCha
         hasResponded = false;
 
         this.result = result
-        if (call.method == "initiateTransaction") {
-            val app: String? = call.argument("app")
-            val pa: String? = call.argument("pa")
-            val pn: String? = call.argument("pn")
-            val mc: String? = call.argument("mc")
-            val tr: String? = call.argument("tr")
-            val tn: String? = call.argument("tn")
-            val am: String? = call.argument("am")
-            val cu: String? = call.argument("cu")
 
-            try {
-                val uriBuilder = Uri.Builder()
-                uriBuilder.scheme("upi").authority("pay")
-                uriBuilder.appendQueryParameter("pa", pa)
-                uriBuilder.appendQueryParameter("pn", pn)
-                uriBuilder.appendQueryParameter("tr", tr)
-                uriBuilder.appendQueryParameter("am", am)
-                uriBuilder.appendQueryParameter("cu", cu)
-                uriBuilder.appendQueryParameter("mode", "intent")
-                if (mc != null) {
-                    uriBuilder.appendQueryParameter("mc", mc)
-                }
-                if (tn != null) {
-                    uriBuilder.appendQueryParameter("tn", tn)
-                }
-
-                val uri = uriBuilder.build()
-                val intent = Intent(Intent.ACTION_VIEW, uri)
-                intent.setPackage(app)
-
-
-                if (intent.resolveActivity(activity.packageManager) == null) {
-                    this.success("activity_unavailable")
-                    return
-                }
-
-                activity.startActivityForResult(intent, requestCodeNumber)
-                this.result = result
-            } catch (ex: Exception) {
-                Log.d("upi_pay", ex.toString())
-                this.success("failed_to_open_app")
-            }
-        } else {
-            result.notImplemented()
+        when (call.method) {
+            "initiateTransaction" -> this.initiateTransaction(call)
+            "getInstalledUPIApps" -> this.getInstalledUPIApps()
+            else -> result.notImplemented()
         }
+    }
+
+    private fun initiateTransaction(call: MethodCall) {
+        val app: String? = call.argument("app")
+        val pa: String? = call.argument("pa")
+        val pn: String? = call.argument("pn")
+        val mc: String? = call.argument("mc")
+        val tr: String? = call.argument("tr")
+        val tn: String? = call.argument("tn")
+        val am: String? = call.argument("am")
+        val cu: String? = call.argument("cu")
+
+        try {
+            val uriBuilder = Uri.Builder()
+            uriBuilder.scheme("upi").authority("pay")
+            uriBuilder.appendQueryParameter("pa", pa)
+            uriBuilder.appendQueryParameter("pn", pn)
+            uriBuilder.appendQueryParameter("tr", tr)
+            uriBuilder.appendQueryParameter("am", am)
+            uriBuilder.appendQueryParameter("cu", cu)
+            uriBuilder.appendQueryParameter("mode", "intent")
+            if (mc != null) {
+                uriBuilder.appendQueryParameter("mc", mc)
+            }
+            if (tn != null) {
+                uriBuilder.appendQueryParameter("tn", tn)
+            }
+
+            val uri = uriBuilder.build()
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            intent.setPackage(app)
+
+            if (intent.resolveActivity(activity.packageManager) == null) {
+                this.success("activity_unavailable")
+                return
+            }
+
+            activity.startActivityForResult(intent, requestCodeNumber)
+        } catch (ex: Exception) {
+            Log.d("upi_pay", ex.toString())
+            this.success("failed_to_open_app")
+        }
+    }
+
+    private fun getInstalledUPIApps() {
+        val uriBuilder = Uri.Builder()
+        uriBuilder.scheme("upi").authority("pay")
+        uriBuilder.appendQueryParameter("pa", "abc@ybl")
+        uriBuilder.appendQueryParameter("pn", "abc")
+        uriBuilder.appendQueryParameter("tr", "123")
+        uriBuilder.appendQueryParameter("am", "10")
+        uriBuilder.appendQueryParameter("cu", "INR")
+        uriBuilder.appendQueryParameter("mode", "intent")
+
+        val uri = uriBuilder.build()
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+
+        val packageManager = activity.packageManager
+
+        val activities = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY)
+        val activityResponse = activities.map {
+            val icon = packageManager.getApplicationIcon(it.resolvePackageName)
+
+             mapOf(
+                    "packageName" to it.resolvePackageName,
+                    "icon" to it.icon,
+                    "priority" to it.priority,
+                    "preferredOrder" to it.preferredOrder
+            )
+
+        }
+
+        result?.success(activityResponse)
     }
 
     private fun success(o: String) {
